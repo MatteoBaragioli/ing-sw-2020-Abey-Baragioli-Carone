90d8773b996ca0a5fafd815123bcac5c9ce84ea5
Jitendra Abey Gunawardena <jitendra.abey@mail.polimi.it>
2020-04-17 17:50:00 +0200

    Card Creators

---
+++ src/main/java/it/polimi/ingsw/server/model/CardConstructor.java
@@ -0,0 +1,122 @@
+package it.polimi.ingsw.server.model;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import it.polimi.ingsw.server.model.godPowers.*;
+
+import java.io.File;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CardConstructor {
+    private List<ProtoCard> protoCards = loadCardsFromFile();
+
+    /**
+     * This method loads a Protocard List from a file
+     * @return List of Protocards
+     */
+    public List<ProtoCard> loadCardsFromFile(){
+        String filename = "src" + File.separator + "main" + File.separator + "resources"+ File.separator + "GodCards.json";
+        List<ProtoCard> protoCards = null;
+        try {
+            Type listType = new TypeToken<List<ProtoCard>>() {}.getType();
+            protoCards = new Gson().fromJson(new FileReader(filename), listType);
+            String jsonObject = new Gson().toJson(protoCards);
+            protoCards = new Gson().fromJson(jsonObject, listType);
+
+        } catch (Exception FileNotFoundException) {
+            //
+        }
+        return protoCards;
+    }
+
+    /**
+     * This method loads the fx used by the protocards identifying them by their name
+     * @return HashMap
+     */
+    public Map<String, TurnSequenceModifier> loadFX() {
+        Map<String, TurnSequenceModifier> fx = new HashMap<>();
+        fx.put("DoNothing", new DoNothing());
+        fx.put("Swap", new SwapPower());
+        fx.put("AddMoveNotStartingBox", new AddMoveNotStartingBoxPower());
+        fx.put("OpponentsCantMoveUpIfPlayerMovesUp", new OpponenentsCantMoveUpIfPlayerMovesUpPower());
+        fx.put("BuildDomeEverywhere", new BuildDomeEverywherePower());
+        fx.put("AddBuildNotSameBox", new AddBuildNotSameBoxPower());
+        fx.put("AddBuildOnSameBox", new AddBuildOnSameBoxPower());
+        fx.put("PushAdjacentOpponent", new PushAdjacentOpponentPower());
+        fx.put("AddBuildBeforeIfNotMoveUp", new AddBuildBeforeMoveIfNotMoveUpPower());
+        fx.put("RemoveAdjacentBlock", new RemoveAdjacentBlockPower());
+        fx.put("AddBuildNotEdge", new AddBuildNotEdgePower());
+        fx.put("AddThreeBuildsToUnmovedWorker", new AddThreeBuildsToUnmovedWorkerIfOnGroundPower());
+        fx.put("BuildUnderYourself", new BuildUnderYourselfPower());
+
+        return fx;
+    }
+
+    /**
+     * This method returns the actions needed to create the card
+     * @param protoCard the card I need to create
+     * @return List of TurnSequenceModifiers
+     */
+    public List<TurnSequenceModifier> loadActions(ProtoCard protoCard) {
+        List<TurnSequenceModifier> actions = new ArrayList<>();
+        Map<String, TurnSequenceModifier> powers = loadFX();
+
+        for (String power: protoCard.actions())
+            actions.add(powers.get(power));
+
+        return actions;
+    }
+
+    /**
+     * This method returns the WinCondition needed to create the card
+     * @param protoCard the card I need to create
+     * @return WinCondition
+     */
+    public WinCondition loadWinCondition(ProtoCard protoCard) {
+        Map<String, WinCondition> winConditions = new HashMap<>();
+        winConditions.put("Standard", null);
+        winConditions.put("MoveTwoLevelsDown", new MoveTwoLevelsDownWin());
+        winConditions.put("TowerCountWin", new TowerCountWin(protoCard.winParameter()));
+        return winConditions.get(protoCard.winCondition());
+    }
+
+    /**
+     * This method returns the SetUpCondition needed to create the card
+     * @param protoCard the card I need to create
+     * @return SetUpCondition
+     */
+    public SetUpCondition loadSetUpCondition(ProtoCard protoCard) {
+        Map<String, SetUpCondition> setUpConditions = new HashMap<>();
+        setUpConditions.put("NoSetUp", new NoSetUpCondition());
+        return setUpConditions.get(protoCard.setUpCondition());
+    }
+
+    /**
+     * This method returns the opponent actions needed to create the card
+     * @param protoCard the card I need to create
+     * @return List of TurnSequenceModifiers
+     */
+    public List<TurnSequenceModifier> loadFXOnOpponents(ProtoCard protoCard) {
+        List<TurnSequenceModifier> fx = new ArrayList<>();
+        Map<String, TurnSequenceModifier> effects = loadFX();
+
+        for (String power: protoCard.fxOnOpponent())
+            fx.add(effects.get(power));
+
+        return fx;
+    }
+
+    /**
+     * This method generates a GodCard from a Protocard
+     * @param protoCard the Protocard I want to create
+     * @return GodCard
+     */
+    public GodCard createCard(ProtoCard protoCard) {
+        return new GodCard(protoCard.name(), protoCard.id(), loadActions(protoCard), loadWinCondition(protoCard), loadSetUpCondition(protoCard), loadFXOnOpponents(protoCard));
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/server/model/ProtoCard.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.server.model;
+
+public class ProtoCard {
+    private final String name;
+    private final int id;
+    private final String[] actions;
+    private final String winCondition;
+    private final int winParameter;
+    private final String setUpCondition;
+    private final String[] fxOnOpponent;
+
+    public ProtoCard(String name, int id, String[] actions, String winCondition, int winParameter, String setUpCondition, String[] fxOnOpponent, int[] opponentParameters) {
+        this.name = name;
+        this.id = id;
+        this.actions = actions;
+        this.winCondition = winCondition;
+        this.winParameter = winParameter;
+        this.setUpCondition = setUpCondition;
+        this.fxOnOpponent = fxOnOpponent;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String[] actions() {
+        return actions;
+    }
+
+    public String winCondition() {
+        return winCondition;
+    }
+
+    public int winParameter() {
+        return winParameter;
+    }
+
+    public String setUpCondition() {
+        return setUpCondition;
+    }
+
+    public String[] fxOnOpponent() {
+        return fxOnOpponent;
+    }
+}
